adfd9a12c81a34906218e74ee94f4d5b0ec6c75c
Albert Schimpf <git@schimpf.link>
2019-03-11 12:08:59 +0900

    removed stat sup

--- src/riak_core_sup.erl
+++ src/riak_core_sup.erl
@@ -72,8 +72,8 @@ init([]) ->
                   ?CHILD(riak_core_capability, worker),
                   ?CHILD(riak_core_gossip, worker),
                   ?CHILD(riak_core_claimant, worker),
-                  ?CHILD(riak_core_table_owner, worker),
-                  ?CHILD(riak_core_stat_sup, supervisor)
+                  ?CHILD(riak_core_table_owner, worker)
+%%                  ?CHILD(riak_core_stat_sup, supervisor)
                  ]),
 
     {ok, {{one_for_one, 10, 10}, Children}}.
